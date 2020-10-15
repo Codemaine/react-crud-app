26987688a86a79013c70ca0c5fc01f6a92bdb945
Jermaine <jermaineantwi22@gmail.com>
2020-10-15 19:47:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'minimatch'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Books extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      books: [
+        {
+          name: 'Harry Potter',
+          author: 'Micheal',
+          description: 'This is the book based on the Harry Potter movie'
+        },
+        {
+          name: 'BFG',
+          author: 'Roald Dalph',
+          description: 'This book is amazing.'
+        }
+      ],
+      name: '',
+      author: '',
+      description: ''
+    }
+  }
+  
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newbook = {
+      name: this.state.name,
+      author: this.state.author,
+      description: this.state.description
+    }
+   this.setState({
+     books: [...this.state.books, newbook],
+     name: '',
+     author: '',
+     description: ''
+   })
+   
+  }
+
+  render() {
+    return (
+      <div style={{margin: '2rem'}}>
+        <form onSubmit={this.handleSubmit}>
+          <div style={{display: 'flex', flexDirection: 'column', width: '34vh'}}>
+            <label>Name</label>
+          <input type="text" name="name" onChange={this.handleChange} />
+          <label>Author</label>
+          <input type="text" name="author" onChange={this.handleChange} />
+          <label>Description</label>
+          <textarea name="description" onChange={this.handleChange} ></textarea>
+          </div>
+          <button class="button" type="submit">Submit</button>
+                  </form>
+                  <table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Author</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    {this.state.books.map ((books, id) => {
+      return (
+       <tr>
+         <td>{books.name}</td>
+         <td>{books.author}</td>
+      <td>{books.description}</td>
+       </tr>
+      )
+    })}
+  </tbody>
+</table>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Books;
